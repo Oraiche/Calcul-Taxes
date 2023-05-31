c06e81c52b2bdd1021ca8dc1509ff663bdc8d079
Mohammed Oraiche <mohammed.oraiche@gmail.com>
2023-05-31 12:06:16 +0100

    Ajout d un formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FactureComponent} from "./facture/facture.component";
 import {PaniersComponent} from "./paniers/paniers.component";
+import { NewProduitComponent } from './new-produit/new-produit.component';
+import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     PaniersComponent,
-    FactureComponent
+    FactureComponent,
+    NewProduitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
